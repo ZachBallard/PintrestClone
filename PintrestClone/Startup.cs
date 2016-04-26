bcd0acf5600cd555c66774bebc93514635d324a9
Zach Ballard <zbginji@gmail.com>
2016-04-26 13:01:46 -0500

    Add project files.

---
+++ PintrestClone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PintrestClone.Startup))]
+namespace PintrestClone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
